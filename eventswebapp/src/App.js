962393ebfd994c33046e30dbc75435f20a41b3c3
unknown <ainsley.dalton2016@gmail.com>
2023-02-21 16:17:56 -0500

    start login screen

---
+++ eventswebapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginScreen from './screens/loginScreen';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Welcome to your College Events :)
+    //     </p>
+    //   </header>
+    // </div>
+    <LoginScreen/>
+  );
+}
+
+export default App;
---
+++ eventswebapp/src/screens/loginScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/login.css';
+
+export default function LoginScreen() {
+    return (
+        <body>
+            <div class="Login">
+                <h1>Login</h1>
+                <p>Please enter your userid and password</p>
+            </div>
+        </body>
+    );
+}
