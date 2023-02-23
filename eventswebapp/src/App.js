01c452126d118347c29f4477cc17e1378be2f276
Shayne Tyrell <shaynet0809@gmail.com>
2023-02-23 16:18:53 -0500

    login

--- eventswebapp/src/App.js
+++ eventswebapp/src/App.js
@@ -1,17 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginScreen from './screens/loginScreen';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Welcome to your College Events :)
-    //     </p>
-    //   </header>
-    // </div>
     <LoginScreen/>
   );
 }
--- eventswebapp/src/screens/loginScreen.js
+++ eventswebapp/src/screens/loginScreen.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import '../styles/login.css';
+import React, { useState } from 'react'
+import '../App.css'
 
 export default function LoginScreen() {
+    const [userId, setUserId] = useState('');
+    const [password, setPassword] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleUserId = (e) => {
+        setUserId(e.target.value);
+    }
+    
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (userId === '' || password === '') {
+            setError(true);
+        }
+        else {
+            setSubmitted(true);
+            setError(false);
+        }
+    }
+    
     return (
-        <body>
+        <body style={{
+            textAlign: 'center', 
+            backgroundColor: '#DCCCBB',
+            height: '100vh',
+            minHeight:'100vh',
+        }}>
+            <h1>Event Manager</h1>
+            <p>Please log-in with your UserID and password</p>
             <div class="Login">
-                <h1>Login</h1>
-                <p>Please enter your userid and password</p>
+                <form>
+                    <label className="label">UserID: </label>
+                    <input className="input" value={userId} type="text" onChange ={handleUserId} />
+                    <br/>
+                    <label className='label'>Password: </label>
+                    <input className="input" value={password} type="text" onChange={handlePassword}/>
+                    <br/>
+                    <br/>
+                    <button className="btn" type="submit" onClick={handleSubmit}>Submit</button>
+                </form>
             </div>
         </body>
     );
