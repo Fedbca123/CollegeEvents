75a02163be8fef48ab83aac1db41360c0594ff4b
Omar Shalaby <omarshalaby911@knight.ucf.edu>
2023-04-14 23:58:38 -0400

    more work done

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
+const mysql = require('mysql');
 
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '4710COPP',
+  database: 'COP4710'
+})
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows[0].solution);
+})
+
+// connection.end();
 
 const app = express();
 const port = process.env.PORT || 5555;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
 const userRouter = require('./routes/users');
 app.use('/users', userRouter);
 
 // using ejs as templating engine
 app.set('view engine', 'ejs');
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
 
 
 app.listen(port, () => {
